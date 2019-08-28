9fb82a184bb21f7f3f169f2c183e20e6aba0402a
jacob <jas0155@auburn.edu>
2019-08-27 21:26:36 -0500

    First Commit

--- src/main/java/Finder.java
+++ src/main/java/Finder.java
@@ -1,2 +1,26 @@
 public class Finder {
+
+    // Function to find the minimum value given an integer array.
+    // Empty/null arrays should return null.
+    public static int indMin(int[] intArray) {
+
+        // check if array is null or empty.
+        if (intArray == null || intArray.length == 0) {
+            return Integer.parseInt(null);
+        }
+
+        // initialize minimum value.
+        int minValue = intArray[0];
+
+        // loop through remaining elements.
+        for (int i = 1; i < intArray.length; i++) {
+            if (intArray[i] < minValue) {
+                    minValue = intArray[i];
+            }
+        }
+
+        // return final value
+        return minValue;
+    }
+
 }
